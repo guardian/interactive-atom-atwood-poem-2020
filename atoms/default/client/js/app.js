68a17c25408fdffeefb49db644f3b9f8173653e3
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-11-05 15:26:23 +0100

    header and meta

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,24 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
+
+function changeSeason() {
+  const nextSeason = {
+    'autumn': 'winter',
+    'winter': 'spring',
+    'spring': 'summer',
+    'summer': 'autumn'
+  }
+
+  const currentSeason = document.body.dataset.season;
+  if (currentSeason) {
+    document.body.dataset.season = nextSeason[currentSeason];
+  } else {
+    document.body.dataset.season = Object.keys(nextSeason)[0];
+  }
+
+}
+
+changeSeason();
+setInterval(() => {
+  changeSeason();
+}, 24000)
--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -2,21 +2,44 @@ var el = document.createElement('script');
 el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
 
+function init() {
+  setupHeader();
+}
+init();
 
+function setupHeader() {
+  const headerEl = document.querySelector('.content__headline');
+  splitHeadline(headerEl);
+  injectImage(headerEl);
+}
+
+function splitHeadline(headerEl) {
+  const headerText = headerEl.innerText.split(': ');
+  console.log("HIHIHIH")
+  console.log(headerText);
+  headerEl.innerHTML = `<span class='h'>${headerText[0]}</span><span class='b'>${headerText[1]}</span>`;
+}
+
+function injectImage(headerEl) {
+  let pictureEl = document.createElement('picture');
+  pictureEl.classList.add('bg-illustration')
+  pictureEl.innerHTML = `<source srcset='https://uploads.guim.co.uk/2020/11/05/Opener.jpg' type='image/jpeg'><source srcset='https://uploads.guim.co.uk/2020/11/05/Opener.webp' type='image/webp'><source srcset='' type='image/avif'><img src='https://uploads.guim.co.uk/2020/11/05/Opener.jpg'>`;
+  headerEl.parentElement.appendChild(pictureEl);
+}
 
 setTimeout(() => {
-  if (window.resize) {  
+  if (window.resize) {
     const html = document.querySelector('html')
     const body = document.querySelector('body')
-  
+
     html.style.overflow = 'hidden'
     html.style.margin = '0px'
     html.style.padding = '0px'
-  
+
     body.style.overflow = 'hidden'
     body.style.margin = '0px'
     body.style.padding = '0px'
-  
-  window.resize()
+
+    window.resize()
   }
-},100)
+}, 100)
